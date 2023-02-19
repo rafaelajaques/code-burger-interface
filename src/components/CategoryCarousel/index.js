a65ed175fc467f9832c08d80ba050652e1094578
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-02-18 22:14:13 -0300

    Criado carrossel de categorias

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,22 +1,45 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/button-has-type */
+import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Carousel from 'react-elastic-carousel';
 import Category from '../../assets/category.png';
 import api from '../../services/api';
-import { Container, CategoryImg } from './style';
+import { Container, CategoryImg, ContainerItems, Image, Button } from './style';
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('categories');
+      const { data } = await api.get('categories');
 
-      console.log(response);
+      setCategories(data);
     }
 
     loadCategories();
   }, []);
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 },
+  ];
+
   return (
     <Container>
       <CategoryImg src={Category} alt="logo da categoria" />
+
+      <Carousel itemsToShow={4} style={{ width: '90%' }} breakPoints={breakPoints}>
+        {categories &&
+          categories.map((category) => (
+            <ContainerItems key={category.id}>
+              <Image src={category.url} alt="imagem da categoria" />
+              <Button>{category.name}</Button>
+            </ContainerItems>
+          ))}
+      </Carousel>
     </Container>
   );
 }
