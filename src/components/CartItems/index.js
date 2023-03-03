55354a6bfbe2f1a09cd627b681c17f607b68fcce
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-03-03 14:48:57 -0300

    Botões de quantidade estilizados

--- src/components/CartItems/index.js
+++ src/components/CartItems/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
-import { Container, Header, Body } from './style';
+import { Container, Header, Body, EmptyCart } from './style';
 import { useCart } from '../../hooks/CartContext';
 import fomatCurrency from '../../utils/formatCurrency';
 
@@ -14,19 +14,26 @@ export function CartItems() {
         <p />
         <p>Item</p>
         <p>Valor</p>
-        <p>Quantidade</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
         <p>Total</p>
       </Header>
-      {cartProducts &&
+      {cartProducts && cartProducts.length > 0 ? (
         cartProducts.map((product) => (
           <Body key={product.id}>
             <img src={product.url} alt="imagem do produto" />
             <p>{product.name}</p>
             <p>{fomatCurrency(product.price)}</p>
-            <p>{product.quantity}</p>
+            <div className="quantity-container">
+              <button type="button">-</button>
+              <p>{product.quantity}</p>
+              <button type="button">+</button>
+            </div>
             <p>{fomatCurrency(product.quantity * product.price)}</p>
           </Body>
-        ))}
+        ))
+      ) : (
+        <EmptyCart> Carrinho vazio</EmptyCart>
+      )}
     </Container>
   );
 }
