0047a386387b30a33b40b44343325b5e9a5a2687
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-03-04 17:51:24 -0300

    Criada a lógica do resumo do carrinho

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,23 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Container } from './style';
 import { Button } from '../Button';
+import formatCurrency from '../../utils/formatCurrency';
+import { useCart } from '../../hooks/CartContext';
 
 export function CartResume() {
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [deliveryTax] = useState(5);
+
+  const { cartProducts } = useCart();
+
+  useEffect(() => {
+    const sumAllItems = cartProducts.reduce(
+      (acc, current) => current.price * current.quantity + acc,
+      0
+    );
+
+    setFinalPrice(sumAllItems);
+  }, [cartProducts, deliveryTax]);
+
   return (
     <div>
       <Container>
         <div className="container-top">
           <h2 className="title">Resumo do pedido</h2>
           <p className="items">Itens</p>
-          <p className="items-price">R$10,00</p>
+          <p className="items-price">{formatCurrency(finalPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
-          <p className="delivery-tax-price">R$10,00</p>
+          <p className="delivery-tax-price">{formatCurrency(deliveryTax)}</p>
         </div>
         <div className="container-bottom">
           <p>Total</p>
-          <p>R$10,00</p>
+          <p>{formatCurrency(finalPrice + deliveryTax)}</p>
         </div>
       </Container>
       <Button style={{ width: '100%', marginTop: 30 }}>Finalizar Pedido</Button>
