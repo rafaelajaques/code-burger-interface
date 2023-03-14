086dce08a644d3a374ec581f68194fe2ca535848
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-03-14 18:37:33 -0300

    Tabela de pedidos criada

---
+++ src/containers/Admin/Orders/style.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #e5e5e5;
+  min-height: 100vh;
+`;
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -2,11 +2,12 @@
 
 import React from 'react';
 import { Container } from './style';
+import Orders from './Orders';
 
 export function Admin() {
   return (
     <Container>
-      <h1>Admin</h1>
+      <Orders />
     </Container>
   );
 }
